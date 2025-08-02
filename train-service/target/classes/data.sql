1d47a307464e41812445daa523aec1e1159f9cda
Ruben <ruben.aguilarcaste@gmail.com>
2025-08-02 17:07:46 +0200

    Remove target folder from Git tracking - all

--- train-service/target/classes/data.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO trains (id, name, total_seats) VALUES (1, 'Tren AVE', 200);
-INSERT INTO trains (id, name, total_seats) VALUES (2, 'Tren Talgo', 150);
-INSERT INTO trains (id, name, total_seats) VALUES (3, 'Tren Regional', 100);
